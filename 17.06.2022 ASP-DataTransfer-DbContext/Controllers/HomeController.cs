7f633da393535d229d5ba05eedffff0b734d3410
Elgun Guluzada <elgunpg@code.edu.az>
2022-06-20 16:48:47 +0400

    feat: Group and Student Models Props Added

--- 17.06.2022 ASP-DataTransfer-DbContext/Controllers/HomeController.cs
+++ 17.06.2022 ASP-DataTransfer-DbContext/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _17._06._2022_ASP_DataTransfer_DbContext.Models;
+using _17._06._2022_ASP_DataTransfer_DbContext.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace _17._06._2022_ASP_DataTransfer_DbContext.Controllers
 {
@@ -10,7 +13,27 @@ namespace _17._06._2022_ASP_DataTransfer_DbContext.Controllers
             ViewData["Surname"] = "Guluzade";
             TempData["Age"] = 29;
 
-            return RedirectToAction(nameof(About));
+
+            List<Student> students = new List<Student>
+            {
+                new Student { Id =0, Name="Elgun", Surname="Guluzade", Age=29},
+                new Student { Id =1, Name="Hesen", Surname="Hesenzade", Age=23},
+                new Student { Id =2, Name="Eli", Surname="Abbasov", Age=25}
+            };
+
+            List<Group> groups = new List<Group>
+            {
+                new Group { Id =0, GroupName="P116"},
+                new Group { Id =1, GroupName="P322"}
+            };
+
+            HomeVM home = new HomeVM();
+            home.Students = students;
+            home.Groups = groups;
+
+
+            //return RedirectToAction(nameof(About));
+            return View(home);
         }
         public IActionResult About()
         {
